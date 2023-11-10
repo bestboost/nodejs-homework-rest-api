80740aa97270b84116bd49b1b7bd10c88172b00d
Darya Hudzovska <102170750+bestboost@users.noreply.github.com>
2023-11-10 17:55:06 +0200

    setting router

--- app.js
+++ app.js
@@ -1,25 +1,41 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
 
-const contactsRouter = require('./routes/api/contacts')
+const contactsRouter = require("./routes/api/contacts");
 
-const app = express()
+const app = express();
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
-app.use('/api/contacts', contactsRouter)
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+
+// підключення проміжного ПЗ
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+//обробка статичних ресурсів
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/api/contacts", contactsRouter);
 
 app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
-})
+  res.status(404).json({ message: "Not found" });
+});
 
 app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message })
-})
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
-module.exports = app
+  // render the error page
+  res.status(500).json({ message: err.message });
+});
+
+module.exports = app;
