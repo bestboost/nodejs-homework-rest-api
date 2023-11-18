8d2f9bac89b26f8b4ec8467bfa58a90c435d0a17
Darya Hudzovska <102170750+bestboost@users.noreply.github.com>
2023-11-18 17:24:24 +0200

    back to strind DB_C...

--- server.js
+++ server.js
@@ -2,14 +2,15 @@ import mongoose from "mongoose";
 // TzsGIRF1cyfW9cRD
 import app from "./app.js";
 
-const { DB_CONTACTS, PORT = 3000 } = process.env;
-
+// const { DB_CONTACTS, PORT = 3000 } = process.env;
+const DB_CONTACTS =
+  "mongodb+srv://Darya:TzsGIRF1cyfW9cRD@cluster0.llnpj3k.mongodb.net/db-contacts?retryWrites=true&w=majority";
 mongoose.set("strictQuery", true);
 
 mongoose
   .connect(DB_CONTACTS)
   .then(() => {
-    app.listen(PORT);
+    app.listen(3000);
   })
   .catch((error) => {
     console.log(error.message);
