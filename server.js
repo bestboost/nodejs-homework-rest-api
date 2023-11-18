1488afad976b0534bddd4a6b33b1056a9ad5928d
Darya Hudzovska <102170750+bestboost@users.noreply.github.com>
2023-11-18 17:55:23 +0200

    add node v

--- server.js
+++ server.js
@@ -5,12 +5,11 @@ import app from "./app.js";
 // const { DB_CONTACTS, PORT = 3000 } = process.env;
 const DB_CONTACTS =
   "mongodb+srv://Darya:TzsGIRF1cyfW9cRD@cluster0.llnpj3k.mongodb.net/db-contacts?retryWrites=true&w=majority";
-mongoose.set("strictQuery", true);
 
 mongoose
   .connect(DB_CONTACTS)
   .then(() => {
-    app.listen(3000);
+    app.listen(3000, () => console.log("Database connection successful"));
   })
   .catch((error) => {
     console.log(error.message);
