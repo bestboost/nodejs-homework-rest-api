4c64896cbec114d68eb202c98ef845d92fcd40f2
Darya Hudzovska <102170750+bestboost@users.noreply.github.com>
2023-12-06 20:28:23 +0200

    step 3

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -17,13 +17,19 @@ const register = async (req, res) => {
   }
 
   const hashPassword = await bcrypt.hash(password, 10);
+  const avatarURL = gravatar.url(email);
 
-  const newUser = await User.create({ ...req.body, password: hashPassword });
+  const newUser = await User.create({
+    ...req.body,
+    password: hashPassword,
+    avatarURL,
+  });
 
   res.status(201).json({
     user: {
       email: newUser.email,
       subscription: newUser.subscription,
+      avatarURL: newUser.avatarURL,
     },
   });
 };
